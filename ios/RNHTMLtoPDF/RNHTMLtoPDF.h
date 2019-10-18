8f5dc5d9b3cf8151f5f21e3aa3675192eb9b0f1d
Gaurav Patel <patelgaurav4u@gmail.com>
2019-10-18 11:20:06 +0530

    first commit

---
+++ ios/RNHTMLtoPDF/RNHTMLtoPDF.h
@@ -0,0 +1,9 @@
+
+//  Created by Gaurav on 10/15/19.
+
+#import <React/RCTView.h>
+#import <React/RCTBridgeModule.h>
+#import <WebKit/WebKit.h>
+@interface RNHTMLtoPDF : RCTView <RCTBridgeModule, WKNavigationDelegate>
+
+@end
